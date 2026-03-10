4fa4d2ec44206efbb075ae11adb68aafa5f02ee9
Abdullah Saeed <bnbshrbconect@gmail.com>
2026-03-10 08:21:23 +0300

    Add unathroized error detector to rediret user to login page

--- frontend/lib/api/authFetch.ts
+++ frontend/lib/api/authFetch.ts
@@ -6,11 +6,20 @@ export async function authFetch(url: string, options: RequestInit = {}) {
   const cookiesStore = await cookies();
   const token = cookiesStore.get("jwt_token")?.value;
 
-  return fetch(`${API_URL}${url}`, {
+  const res = await fetch(`${API_URL}${url}`, {
     ...options,
     headers: {
       Authorization: `Bearer ${token}`,
       ...options.headers
     }
   });
+
+  if (res.status === 401) {
+    if (typeof window !== "undefined") {
+      window.location.href = "/login";
+    }
+    throw new Error("Unauthorized");
+  }
+
+  return res;
 }
